8facdaf884b00218d3b0faece16c3d5853fba1c8
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-29 19:16:14 -0300

    renomeando

---
+++ expl01.js
@@ -0,0 +1,11 @@
+let nome = "Rodrigo"
+let arr = ["Black", "Rodrigo", "Negão"]
+console.log(nome[3]);
+console.log(arr[1]);
+
+for (let i = 0; i < nome.length; i++){
+    console.log(nome[i])
+}
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
