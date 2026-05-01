7db3366f56074b5413e9c6482d2ce712d4b4ecf5
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-30 21:40:06 -0300

    funcoes js

--- exercicios/05-exer.js
+++ exercicios/05-exer.js
@@ -1,28 +1,8 @@
-const readline = require('node:readline/promises');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-async function rodar() {
-    const nome = await rl.question('Digite o nome: ');
-    const idade = await rl.question('Digite a idade: ');
-    const cargo = await rl.question('Digite o cargo: ');
-
-    const usuario = {
-        nome: nome,
-        idade: idade,
-        cargo: cargo,
-        saudar: function() {
-            console.log(`Olá, meu nome é ${this.nome}`);
-        }
-    };
-
-    console.log('\n--- Listando as CHAVES do objeto ---');
-
-    for (let chave in usuario) {
-        console.log(`Chave encontrada: ${chave}`);
-    }
-    rl.close();
+const usuario = {
+    id: 1,
+    nome: "Carlos",
+    cargo: "Dev"
+};
+for (let i in usuario) {
+    console.log(`${i}: ${usuario[i]}`);
 }
-
-rodar();
